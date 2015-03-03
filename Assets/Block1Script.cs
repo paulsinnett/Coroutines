f46a0ec73d6d23969c3eecd780e2cb23dd29b557
Paul Sinnett <paul.sinnett@gmail.com>
2015-03-03 16:07:06 +0000

    Week 6 example

---
+++ Assets/Block1Script.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block1Script : MonoBehaviour {
+
+	public Transform target;
+	// Use this for initialization
+	void Start () {
+	
+		StartCoroutine (mySpecialScript ());
+	}
+	
+	IEnumerator mySpecialScript () {
+
+		for (int i = 0; i < 100; ++i) {
+			transform.Translate(transform.up * Time.deltaTime);
+			yield return null;
+		}
+
+		yield return new WaitForSeconds (1);
+
+		target.SendMessage ("DoYourThing");
+
+		for (int i = 0; i < 100; ++i) {
+			transform.Translate(transform.right * Time.deltaTime);
+			yield return null;
+		}
+
+		for (int i = 0; i < 100; ++i) {
+			transform.Translate(transform.up * -Time.deltaTime);
+			yield return null;
+		}
+	}
+}
---
+++ Assets/RedBlockScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RedBlockScript : MonoBehaviour {
+
+	// Use this for initialization
+	void DoYourThing () {
+		
+		StartCoroutine (changeSize ());
+	}
+	
+	IEnumerator changeSize () {
+		
+		for (int i = 0; i < 100; ++i) {
+			transform.localScale += Vector3.one * Time.deltaTime;
+			yield return null;
+		}
+		
+		yield return new WaitForSeconds (1);
+		
+		for (int i = 0; i < 100; ++i) {
+			transform.localScale -= Vector3.one * Time.deltaTime;
+			yield return null;
+		}	
+	}
+}
